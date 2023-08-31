8501a41462f591a618d08f321f4a14ccce898924
al-imam <alimam01828@gmail.com>
2023-08-31 14:02:11 +0600

    implement /generate/itenary/ to send itenary pdf

--- main.py
+++ main.py
@@ -59,3 +59,63 @@ async def generate_pdf(request: Request):
             },
         )
 
+
+
+@app.post("/generate/itenary/")
+async def generate_pdf(request: Request):
+    try:
+        data = await request.json()
+
+        validate(data, itenary_schema)
+
+        pdf_buffer = BytesIO()
+
+        try:
+            content = get_itenary_html(data)
+            pisa.CreatePDF(content, dest=pdf_buffer)
+            pdf_buffer.seek(0)
+
+        except Exception as e:
+            raise HTTPException(
+                status_code=500,
+                detail={
+                    "message": f"PDF generation error: {str(e)}",
+                    "code": "pdf-gen-error",
+                },
+            )
+
+        return StreamingResponse(pdf_buffer, media_type="application/pdf")
+
+    except ValidationError as e:
+        raise HTTPException(
+            status_code=400,
+            detail={
+                "message": "Request doesn't contain valid data!",
+                "expected-shape": {
+                    "guests": [{"name": "string", "passport_no": "string"}],
+                    "itenary": [
+                        {"date": "2023-09-01", "from": "string", "to": "string"}
+                    ],
+                },
+                "provided": data,
+                "code": "invalid-request",
+            },
+        )
+
+    except ValueError:
+        raise HTTPException(
+            status_code=400,
+            detail={
+                "message": "Request body is not valid JSON!",
+                "code": "invalid-json",
+            },
+        )
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail={
+                "message": "Internal Server Error",
+                "code": "server-break",
+            },
+        )
