051a5b08bfb0202c904bb3792d9f7648f84b0a1a
al-imam <alimam01828@gmail.com>
2023-09-01 19:38:40 +0600

    change underline_and_space function

--- src/util.py
+++ src/util.py
@@ -7,17 +7,18 @@ def convert_url_to_pdf(url, output_path):
 
     if response.status_code == 200:
         html_content = response.text
-        with open(output_path, 'wb') as pdf_file:
+        with open(output_path, "wb") as pdf_file:
             pisa.CreatePDF(html_content, dest=pdf_file)
-            print(f'PDF created at: {output_path}')
+            print(f"PDF created at: {output_path}")
     else:
         print(f"Failed to retrieve HTML content from URL: {url}")
 
 
 def convert_html_to_pdf(html_text, output_path):
-    with open(output_path, 'wb') as pdf_file:
+    with open(output_path, "wb") as pdf_file:
         pisa.CreatePDF(html_text, dest=pdf_file)
-        print(f'PDF created at - {output_path}')
+        print(f"PDF created at - {output_path}")
+
 
 def underline_and_space(value):
-    return f"""<span style="text-decoration: underline; text-decoration-color: black;"><span class='underline-hide'>____</span>{value}<span class='underline-hide'>____</span></span>"""
+    return f"""<span style="text-decoration: underline;" class="element" ><span >____</span>{value}<span>____</span></span>"""
