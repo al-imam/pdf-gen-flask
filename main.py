b53354cf152e94746aa12cd92a460476b0f52d8d
al-imam <alimam01828@gmail.com>
2023-09-03 14:30:37 +0600

    combine everything together

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from datetime import datetime
 from io import BytesIO
 from flask import Flask, request, Response, jsonify
 from jsonschema import ValidationError, validate
-from src.generate_visa_pdf import generate_visa_pdf, get_visa_html
-from src.generate_itenary_pdf import generate_itenary_pdf, get_itenary_html
+from src.generate_visa_pdf import get_visa_html
+from src.generate_itenary_pdf import get_itenary_html
 from xhtml2pdf import pisa
-from src.schema import itenary_schema
+from src.schema import itenary_schema, visa_schema
 
 app = Flask(__name__)
 
@@ -26,21 +26,14 @@ def generate_pdf():
                 400,
             )
 
-        if not (isinstance(data, dict) and data.get("name") and data.get("passport")):
-            return (
-                jsonify(
-                    {
-                        "message": "'name' and 'passport' are required!",
-                        "code": "invalid-req",
-                    }
-                ),
-                400,
-            )
+        validate(data, visa_schema)
 
         pdf_buffer = BytesIO()
 
         try:
-            content = get_visa_html(data.get("name"), data.get("passport"))
+            content = get_visa_html(
+                data.get("name"), data.get("passport")
+            )
             pisa.CreatePDF(content, dest=pdf_buffer)
             pdf_buffer.seek(0)
 
@@ -56,6 +49,18 @@ def generate_pdf():
             )
 
         return Response(pdf_buffer, content_type="application/pdf")
+
+    except ValidationError as e:
+        return (
+            jsonify(
+                {
+                    "message": "Request doesn't contain valid data!",
+                    "error": str(e),
+                    "code": "invalid-request",
+                }
+            ),
+            400,
+        )
 
     except Exception as e:
         return (
@@ -99,8 +104,9 @@ def generate_itenary_pdf_route():
             return (
                 jsonify(
                     {
-                        "message": f"PDF generation error: {str(e)}",
+                        "message": "PDF generation valid",
                         "code": "pdf-gen-error",
+                        "error": str(e),
                     }
                 ),
                 500,
@@ -113,13 +119,7 @@ def generate_itenary_pdf_route():
             jsonify(
                 {
                     "message": "Request doesn't contain valid data!",
-                    "expected-shape": {
-                        "guests": [{"name": "string", "passport_no": "string"}],
-                        "itenary": [
-                            {"date": "2023-09-01", "from": "string", "to": "string"}
-                        ],
-                    },
-                    "provided": data,
+                    "error": str(e),
                     "code": "invalid-request",
                 }
             ),
@@ -139,32 +139,5 @@ def generate_itenary_pdf_route():
         )
 
 
-@app.route("/generate/visa-itenary-test", methods=["GET"])
-def generate_pdf_test():
-    generate_itenary_pdf(
-        {
-            "guests": [
-                {"name": "Al-imam", "passport_no": "43534"},
-            ],
-            "itenary": [
-                {
-                    "date": datetime.today().strftime("%Y-%m-%d"),
-                    "from": "AirPost",
-                    "to": "Hotel valentilo",
-                },
-                {
-                    "date": datetime.today().strftime("%Y-%m-%d"),
-                    "from": "AirPost",
-                    "to": "Hotel valentilo",
-                },
-            ],
-        },
-    )
-
-    generate_visa_pdf("Al-Imam", "345D42343")
-
-    return jsonify({"success": True})
-
-
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
